1d49075809f429f8324ccb233c6efec60f5d14c8
Dykhalkin Mikhail Andreevich <m.dykhalkin@tinkoff.ru>
2018-12-26 15:32:33 +0300

    new version v2

--- hello.go
+++ hello.go
@@ -2,6 +2,11 @@ package hello
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello %s! \n", name)
+func Hello(name string, lang string) (string, error) {
+	switch lang {
+	case "en": return fmt.Sprintf("Hello %s! \n", name), nil
+	case "ru": return fmt.Sprintf("Привет %s! \n", name), nil
+	default:
+		return "", fmt.Errorf("lang %q not supported", lang)
+	}
 }
